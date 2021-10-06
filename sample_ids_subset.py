508d5fc130aa38934c8f4801e18d4c2f961d734d
Phil <pjpb3@comcast.net>
2021-10-06 16:08:56 -0400

    backup

--- sample_ids_subset.py
+++ sample_ids_subset.py
@@ -9,17 +9,14 @@ import re
 #set directories
 seq_directory = '/Users/philipbaldassari/Desktop/zim-cos_ChrXseq/'
 
-vcf_directory = '/Users/philipbaldassari/Desktop/zim-cos/'
+vcf_directory = '/Users/philipbaldassari/Desktop/'
 
 
-#creating pop.txt file, printing in pop IDs by splitting file names at multiple delimiters
+#creating pop.txt file, printing in sample IDs by splitting file names at '_'
 with open('{path}'.format(path = vcf_directory) + 'unsorted_pop.txt', 'w') as pop:
         for file in os.listdir(seq_directory):
                 if file.endswith('.seq'):
-                        pattern = '-|0|1|2|3|4|5|6|7|8|9'
-                        string = file
-                        result = re.split(pattern, string)[0]
-                        pop.write(result + '\n')
+                        pop.write('{filename}'.format(filename = file.split('_')[0]) + '\n')
                 else:
                         continue       
 
