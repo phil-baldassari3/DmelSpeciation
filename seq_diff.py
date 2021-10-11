1be84adb7653fa53a744ecc0aa1d09bf73046d7f
Phil <pjpb3@comcast.net>
2021-10-11 15:35:08 -0400

    backup 10/11

--- seq_diff.py
+++ seq_diff.py
@@ -2,25 +2,25 @@ import os
 import itertools
 import pandas as pd
 
-directory = #set directory end with '/'
+directory = '/Users/philipbaldassari/desktop/seq_diff_test/'
 
-#making dictionary with filenames and data
 data = {}
 for file in os.listdir(directory):
     if file.endswith('.seq'):
-        #opening and reading seq file, putting in dictionary
-        data[file] = open(file, 'r').read()
+        #opening and reading seq file
+        data[file.split('.')[0]] = open(file, 'r').read()
+        
     else:
         continue       
 
 print(data.keys())
  
 
-df = pd.DataFrame(columns = ["Locus", "Aprop", "Tprop", "Cprop", "Gprop"])
+df = pd.DataFrame(columns = ["Locus", "totprop", "Aprop", "Tprop", "Cprop", "Gprop"])
 
 site = 0
 
-for locus in zip(data.get('a.seq'), data.get('b.seq'), data.get('c.seq')): #load samples in this way (need to call the values from the dictionary)
+for locus in zip(data.get('a'), data.get('b'), data.get('c')):
 
     
     site += 1
@@ -31,28 +31,30 @@ for locus in zip(data.get('a.seq'), data.get('b.seq'), data.get('c.seq')): #load
     Gcount = locus.count("G")
     
     bpcount = Acount+Tcount+Ccount+Gcount
-    
+   
     if bpcount == 0:
         bpcount += 1
     else:
         bpcount == bpcount
-        
+       
     Aprop = Acount/bpcount
     Tprop = Tcount/bpcount
     Cprop = Ccount/bpcount
     Gprop = Gcount/bpcount
+
+    totprop = Aprop + Tprop + Cprop + Gprop
     
-    df = df.append({'Locus' : site, 'Aprop' : Aprop, 'Tprop' : Tprop, 'Cprop' : Cprop, 'Gprop' : Gprop}, 
+    df = df.append({'Locus' : site, 'totprop' : totprop, 'Aprop' : Aprop, 'Tprop' : Tprop, 'Cprop' : Cprop, 'Gprop' : Gprop}, 
                 ignore_index = True)
 
 
 
 
-df2 = pd.DataFrame(columns = ["Locus", "Aprop2", "Tprop2", "Cprop2", "Gprop2"])
+df2 = pd.DataFrame(columns = ["Locus", "totprop2", "Aprop2", "Tprop2", "Cprop2", "Gprop2"])
 
 site2 = 0
 
-for locus in zip(data.get('d.seq'), data.get('e.seq'), data.get('f.seq')):
+for locus in zip(data.get('d'), data.get('e'), data.get('f')):
     
     site2 += 1
     
@@ -62,22 +64,26 @@ for locus in zip(data.get('d.seq'), data.get('e.seq'), data.get('f.seq')):
     Gcount2 = locus.count("G")
     
     bpcount2 = Acount2+Tcount2+Ccount2+Gcount2
-    
+  
     if bpcount2 == 0:
         bpcount2 += 1
     else:
         bpcount2 == bpcount2
-        
+       
     Aprop2 = Acount2/bpcount2
     Tprop2 = Tcount2/bpcount2
     Cprop2 = Ccount2/bpcount2
     Gprop2 = Gcount2/bpcount2
+
+    totprop2 = Aprop2 + Tprop2 + Cprop2 + Gprop2
     
-    df2 = df2.append({'Locus' : site2 ,'Aprop2' : Aprop2, 'Tprop2' : Tprop2, 'Cprop2' : Cprop2, 'Gprop2' : Gprop2}, 
+    df2 = df2.append({'Locus' : site2 ,'totprop2' : totprop2, 'Aprop2' : Aprop2, 'Tprop2' : Tprop2, 'Cprop2' : Cprop2, 'Gprop2' : Gprop2}, 
                 ignore_index = True)
 
 
 combined = pd.merge(df, df2, on='Locus')
+
+
 
 combined['Adiff'] = abs(combined['Aprop'] - combined['Aprop2'])
 combined['Tdiff'] = abs(combined['Tprop'] - combined['Tprop2'])
@@ -86,7 +92,10 @@ combined['Gdiff'] = abs(combined['Gprop'] - combined['Gprop2'])
 
 combined['diff_score'] = combined[["Adiff","Tdiff","Cdiff","Gdiff"]].max(axis=1)
 
-filtered = combined[combined.diff_score !=0]
+ridofmissing1 = combined[combined.totprop !=0]
+ridofmissing2 = ridofmissing1[ridofmissing1.totprop2 !=0]
+
+filtered = ridofmissing2[ridofmissing2.diff_score !=0]
 
 print(filtered)
 
